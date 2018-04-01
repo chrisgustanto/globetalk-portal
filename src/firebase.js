9aed9af6d78731454f147bd806a143bf20cc911d
Chris Gustanto <cgustanto@gmail.com>
2018-04-01 00:22:37 -0500

    Updated Google firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
-let config = {
-    apiKey: "AIzaSyAIOU_NcQli8MNBxAN9Oi-DwiDz4WT2FlY",
-    authDomain: "globetalk-portfolio.firebaseapp.com",
-    databaseURL: "https://globetalk-portfolio.firebaseio.com",
-    projectId: "globetalk-portfolio",
-    storageBucket: "",
-    messagingSenderId: "941991011702"
+var config = {
+  apiKey: "AIzaSyC10FYCUiv-3C062C9iVgredK8q3mtjW8c",
+  authDomain: "globe-talk-portal.firebaseapp.com",
+  databaseURL: "https://globe-talk-portal.firebaseio.com",
+  projectId: "globe-talk-portal",
+  storageBucket: "globe-talk-portal.appspot.com",
+  messagingSenderId: "749045233222"
 };
 
 firebase.initializeApp(config);
